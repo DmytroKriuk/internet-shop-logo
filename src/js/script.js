36f9a1172ce0078a131c97867aaec867fff051d2
Dmytro Kriuk <dmytro.kryuk@gmail.com>
2020-11-25 21:56:08 +0200

    feat: end search panel

--- src/js/script.js
+++ src/js/script.js
@@ -182,3 +182,28 @@ menuPageBurger.addEventListener("click", function(e) {
   menuPageBody.classList.toggle('_active');
 });
 
+let searchSelect = document.querySelector('.search-page__title');
+let eachSelect = document.querySelector('.categories-search')
+searchSelect.addEventListener("click", function (e) {
+  searchSelect.classList.toggle('_active');
+  eachSelect.classList.toggle('_active');
+});
+
+let checkboxCategories = document.querySelectorAll('.categories-search__checkbox');
+
+for (let index = 0; index < checkboxCategories.length; index++) {
+    const checkboxCategory = checkboxCategories[index];
+    checkboxCategory.addEventListener("change", function (e) {
+        checkboxCategory.classList.toggle('_active');
+       
+        let checkboxActiveCategories = document.querySelectorAll('.categories-search__checkbox._active');
+
+        if (checkboxActiveCategories.length > 0) {
+          searchSelect.classList.add('_categories');
+          let searchQuantity = searchSelect.querySelector('.search-page__quantity');
+          searchQuantity.innerHTML = searchQuantity.getAttribute('data-text') + ' ' +  checkboxActiveCategories.length;
+        } else {
+          searchSelect.classList.remove('_categories');
+        }
+    });
+}
